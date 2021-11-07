fb279bd2f21a436f88abfea9b76c923add545fc6
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-11-07 17:55:40 +0200

    Made some improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.scss';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Destination from './components/Destination';
-import { Route, Routes, useLocation } from 'react-router-dom';
 import Crew from './components/Crew';
+import './App.scss';
+import homeBc from './assets/home/background-home-desktop.jpg';
+import destinationBc from './assets/destination/background-destination-desktop.jpg';
+import crewBc from './assets/crew/background-crew-desktop.jpg';
+import techBc from './assets/technology/background-technology-desktop.jpg';
 
 function App() {
+  useEffect(() => {
+    const bcImages = [homeBc, destinationBc, crewBc, techBc];
+    bcImages.forEach((picture) => {
+      const img = new Image();
+      img.src = picture;
+    });
+  }, []);
+
   const location = useLocation().pathname.replace('/', '');
   return (
     <div className={`App ${location === '' ? 'home' : location}-bc`}>
--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../sass/_crew.scss';
 import crewData from '../data/data';
 
@@ -19,13 +19,6 @@ export default function Crew() {
           <h5 className="crew__heading heading--fifth">
             <span>02</span>Meet your crew
           </h5>
-          <h4 className="crew__position-name heading--fourth">
-            {memberData.role}
-          </h4>
-          <h3 className="crew__member-name heading--tertiary">
-            {memberData.name}
-          </h3>
-          <p className="crew__member-description body-text">{memberData.bio}</p>
           <ul className="crew__dots-component">
             {crew.map((_, i) => {
               return (
@@ -43,6 +36,13 @@ export default function Crew() {
               );
             })}
           </ul>
+          <h4 className="crew__position-name heading--fourth">
+            {memberData.role}
+          </h4>
+          <h3 className="crew__member-name heading--tertiary">
+            {memberData.name}
+          </h3>
+          <p className="crew__member-description body-text">{memberData.bio}</p>
         </div>
         <img
           src={memberData.images.png}
