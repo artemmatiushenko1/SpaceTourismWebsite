53e2256df57f0071a726e66713577b94476d68ca
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-06 09:53:04 +0200

    Removed useless code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Destination from './components/Destination';
 import Crew from './components/Crew';
 import './App.scss';
-import homeBc from './assets/home/background-home-desktop.jpg';
-import destinationBc from './assets/destination/background-destination-desktop.jpg';
-import crewBc from './assets/crew/background-crew-desktop.jpg';
-import techBc from './assets/technology/background-technology-desktop.jpg';
 import Technology from './components/Technology';
 import { AnimatePresence } from 'framer-motion';
 
 function App() {
   const location = useLocation();
   const locationPathname = location.pathname.replace('/', '');
-
-  useEffect(() => {
-    const bcImages = [homeBc, destinationBc, crewBc, techBc];
-    bcImages.forEach((picture) => {
-      const img = new Image();
-      img.src = picture;
-    });
-  }, []);
 
   return (
     <div
